b40458f4b71adc8b4ffe42c275f6bf2df822dee8
jonghoon <>
2023-01-14 17:26:35 +0900

    불필요한 소스 정리

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,5 @@
 <template>
-  <nav>
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
-  </nav>
-  <router-view/>
+  <MainView/>
 </template>
 
 <style lang="scss">
@@ -14,17 +10,10 @@
   text-align: center;
   color: #2c3e50;
 }
-
-nav {
-  padding: 30px;
-
-  a {
-    font-weight: bold;
-    color: #2c3e50;
-
-    &.router-link-exact-active {
-      color: #42b983;
-    }
-  }
-}
 </style>
+<script>
+import MainView from '@/views/MainView'
+export default {
+  components: { MainView }
+}
+</script>
---
+++ src/views/MainView.vue
@@ -0,0 +1,15 @@
+<template>
+  <p>
+    Admin web page
+  </p>
+</template>
+
+<script>
+export default {
+  name: 'MainView'
+}
+</script>
+
+<style scoped>
+
+</style>
